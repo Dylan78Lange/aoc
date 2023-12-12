19ea8fe515df203a3a6b70031f91ae566b7515e6
Dylan78Lange <langedylan@gmail.com>
2023-12-12 22:40:30 +0100

    Working version with sample strings in code

--- aoc_1.py
+++ aoc_1.py
@@ -11,56 +11,37 @@
 import re
 import string
 
-# Define a function that takes a string as a parameter - not used
-def add_numbers(strings):
-    # Use a regular expression to find all numbers in the string
-    numbers = re.findall(r'\d+', strings)
-    
-    # Check if there are any numbers in the list
+# Define a function that takes a string as a parameter and returns the combined first and last digit of the string
+def combine_first_and_last_numbers(input_string):
+    # Find all numbers in the string using regular expression
+    numbers = [int(num) for num in re.findall(r'\d', input_string)]
+
     if numbers:
-        # Convert the numbers to integers
-        numbers = [int(number) for number in numbers]
-        # Add the first and last numbers in the list
-        result = numbers[0] + numbers[-1]
-        print(f"The sum of the first number {numbers[0]} and last number {numbers[-1]} in the string is: {result}")
-        # Return the result
-        return result
+        # Combine the first and last numbers
+        combined_result = str(numbers[0]) + str(numbers[-1])
+        return int(combined_result)
     else:
         # If no numbers are found, return 0 or handle it as appropriate for your case
-        print("No numbers found in the string.")
         return 0
-    
-# Define a function combining the first digit and the last digit (in that order) to form a single two-digit number
-def combine_first_and_last_digit(strings):
-    # Use a regular expression to find all numbers in the string
-    numbers = re.findall(r'\d', strings)
-    
-    # Check if there are any numbers in the list
-    if numbers:
-        # Convert the numbers to integers
-        numbers = [int(number) for number in numbers]
-        # Combine the first and last digits in the list
-        result = numbers[0] * 10 + numbers[-1]
-        print(f"The combined first number {numbers[0]} and last number {numbers[-1]} in the string is: {result}")
-        # Return the result
-        return result
-    else:
-        # If no numbers are found, return 0 or handle it as appropriate for your case
-        print("No numbers found in the string.")
-        return 0
-    
-    # Define a function that takes a string as a parameter
+
 
 # Define a function that takes a list of strings as a parameter
-def add_numbers_list(strings):  
+def add_numbers_list(strings_list):  
     # Initialize a variable to store the sum
-    sum = 0
+    total_sum = 0
     # Loop through each string in the list
-    for string in strings:
-        # Call the combine_first_and_last_digit function on the string and add the result to the sum
-        sum += combine_first_and_last_digit(string)
-    # Return the sum
-    return sum
+    for string in strings_list:
+        # Call the combine_first_and_last_digit function on the string
+        # and add the sum of the results to the total_sum
+        #total_sum += sum(combine_first_and_last_numbers(string))
+        print(f"The input string is: {string}")
+        print(f"Combined first number and last number:{combine_first_and_last_numbers(string)}")
+        # Change the string to integer so that the total_sum can be calculated
+        
+        total_sum += combine_first_and_last_numbers(string)
+    
+    # Return the total_sum
+    return total_sum
 
 # Define a function to open a file and read its contents which contains the list of strings to be SUM'd
 def open_file(filename):
@@ -69,30 +50,38 @@ def open_file(filename):
         # Read the contents of the file
         contents = file.read()
     # Return the contents
+    print(f"The contents of the file are: {contents}")
     return contents
 
 # Define a function to create a list of string from sample data for development and testing purposes
 def create_strings_list():
-    # Create a list of strings
+    # Manually Create a list of strings - DEBUG CODE
     strings_list = [
         's5sevenxrdfr4mhpstgbjcfqckronesix',
         'nkzjrdqrmpztpqninetwofour1znnkd',
         '3four4',
+        'test123this456string',
         'sfdrtpvspsixsn5zbqmggb8vgkjseight',
         '72666gxzflnsfqmndjdscvqmcqls5'
     ]
     # Return the list of strings
+
     return strings_list
 
 # main block
 
 #answer = 0
 # create a list of strings from sample data for development and testing purposes
-strings = create_strings_list()
+strings_list = create_strings_list()
+
+# use input file instead of sample data for development and testing purposes
+# stringsfromfile = open_file('day1-inputs.txt')
+# strings = open_file('day1-inputs.txt')
+
 # print the list of strings (DEBUG when needed)
 # print(strings) 
 # print the sum of the numbers in each string in the list
-answer = add_numbers_list(strings)
+answer = add_numbers_list(strings_list)
 
 # print "The sum of the first number and last number for each string in the list equals: + result"
 print(f"The sum of all the summed numbers in the strings list equals: {answer}")
